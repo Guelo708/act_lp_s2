05a851e8eb2a2ce2d6939230d3687cd7124f76e6
Guelo708 <mguelo7@gmail.com>
2025-09-19 00:03:42 -0500

    Solucion a ejercicio #1

--- src/main/java/com/example/Ejercicio1.java
+++ src/main/java/com/example/Ejercicio1.java
@@ -2,7 +2,12 @@ package com.example;
 
 public class Ejercicio1 {
     public static void ejercicio1() {
-        System.out.println("Ejercicio 1");
-        // tu código aquí
+        int edad = 25;
+        double altura = 1.75;
+        String nombre = "Miguel";
+        boolean estudiante = true;
+
+        System.out.println("Hola, tu nombre es: " + nombre + " tu edad es: " + edad + " y mides:  "+ altura + " ademas eres estudiante "+ estudiante);
+    
     }
 }
