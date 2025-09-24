bed8e16e170bf41b64fe968b85a136411f477ed3
Guelo708 <mguelo7@gmail.com>
2025-09-23 22:52:26 -0500

    Solución ejercicio numero 3

--- src/main/java/com/example/Ejercicio3.java
+++ src/main/java/com/example/Ejercicio3.java
@@ -2,7 +2,19 @@ package com.example;
 
 public class Ejercicio3 {
     public static void ejercicio3() {
-        System.out.println("Ejercicio 3");
-        // tu código aquí
+        int numeroDeEstudiantes = 30;
+        double promedioCalificaciones = 8.5;
+        String nombreCompleto = "Miguel Eduardo Hernández Hernández";
+        boolean esMayorDeEdad = true;
+
+        //estos nombres son validos porque estoy utilizando el estilo de convencion de codificación lowerCamelCase
+
+
+
+        System.out.println(numeroDeEstudiantes);
+        System.out.println(promedioCalificaciones);
+        System.out.println(nombreCompleto);
+        System.out.println(esMayorDeEdad);
+        
     }
 }
