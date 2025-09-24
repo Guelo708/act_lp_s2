80a7ea2f6a50862145d3b534ed2678647e3fe137
Guelo708 <mguelo7@gmail.com>
2025-09-23 22:34:52 -0500

    Solucion ejercicio numero 2

--- src/main/java/com/example/Ejercicio2.java
+++ src/main/java/com/example/Ejercicio2.java
@@ -2,7 +2,23 @@ package com.example;
 
 public class Ejercicio2 {
     public static void ejercicio2() {
-        System.out.println("Ejercicio 2");
-        // tu código aquí
+    byte valor = 100;
+    short numero = 1000;
+    int numero2 = 50000;
+    long numeroLargo = 1000000L;
+    float valor2 = 3.14f;
+    double valorDecimal = 2.71828;
+    char texto = 'A';
+    boolean empleado = false;
+
+    System.out.println(valor + ", tipo de dato byte ");
+    System.out.println(numero + " ,tipo de dato short ");
+    System.out.println(numero2 + ", tipo de dato int ");
+    System.out.println(numeroLargo+ ", tipo de dato long ");
+    System.out.println(valor2 + ", tipo de dato float ");
+    System.out.println(valorDecimal +", tipo de dato double ");
+    System.out.println(texto + ", tipo de dato texto ");
+    System.out.println(empleado + ", tipo de dato boolean ");
+
     }
 }
