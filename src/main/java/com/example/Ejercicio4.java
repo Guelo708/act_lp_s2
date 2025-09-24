debfe2fe28fcd4a3d8c9e892506fb1d0971f1e35
Guelo708 <mguelo7@gmail.com>
2025-09-23 23:08:35 -0500

    Solución ejercicio núumero 4

--- src/main/java/com/example/Ejercicio4.java
+++ src/main/java/com/example/Ejercicio4.java
@@ -2,7 +2,18 @@ package com.example;
 
 public class Ejercicio4 {
     public static void ejercicio4() {
-        System.out.println("Ejercicio 4");
-        // tu código aquí
+
+        final double PI = 3.14159;
+        final int DIAS_SEMANA = 7;
+        final String NOMBRE_UNIVERSIDAD = "CESDE";
+        int radio = 5;
+        double areaDeUnCirculo = PI * radio;
+
+
+        System.out.println(PI);
+        System.out.println(DIAS_SEMANA);
+        System.out.println(NOMBRE_UNIVERSIDAD);
+        System.out.println("El area de un circulo es: " + areaDeUnCirculo);
+        
     }
 }
